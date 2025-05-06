2a469ca2c65d512ae2bbba7c5690c141fbde6512
Thaanees <thaaneesrafeek11@gmail.com>
2025-05-06 21:40:01 +0530

    WIP: saving local changes before pull

--- app.py
+++ app.py
@@ -2,8 +2,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello, Jenkins CI/CD!"
+def hello():
+    return "Hello from Jenkins pipeline!"
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
