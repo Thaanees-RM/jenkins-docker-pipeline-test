0a3e8221269b34d5e75247db5f21b762cb5fe2dc
Thaanees <thaaneesrafeek11@gmail.com>
2025-05-06 21:54:13 +0530

    Break: simulate pipeline failure

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    return "Hello from Jenkins pipeline!"
+def hello:
+    return "✅ Hello from Jenkins Docker Pipeline!"
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
+
